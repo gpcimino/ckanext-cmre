746a429d32fdcce1699af702604b207ef6648f13
Tobia Di Pisa <tobia.dipisa@geo-solutions.it>
2013-11-21 08:51:08 +0100

    Introducing first CMRE custom template

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+
+setup(
+	name='ckanext-cmre',
+	version=version,
+	description="Ckan cmre theme extension",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Tobia Di Pisa',
+	author_email='tobia.dipisa@geo-solutions.it',
+	url='',
+	license='',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.cmre'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points="""
+        [ckan.plugins]
+	# Add plugins here, eg
+	cmre=ckanext.cmre.plugin:CMREThemePlugin
+	""",
+)
